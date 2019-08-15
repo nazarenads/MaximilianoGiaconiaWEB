adb0adb08e92ca9e61b84e819cd1f4f1accdaa0a
nazarenads <nazarenads@gmail.com>
2019-08-14 21:58:45 -0300

    changed padding from section title

--- contact-form.php
+++ contact-form.php
@@ -9,20 +9,24 @@
 
 		$name = $_POST['contactname'];
 		$email = $_POST['contactemail'];
+		$phone = $_POST['contactphone'];
+		$city = $_POST['contactcity'];
 		$message = $_POST['contactmessage'];
-		
+
 		$formcontent=" From: MiniPO Contact Form $email \n
 		Name: $name \n
 		Email Address: $email \n
+		Phone: $phone \n
+		City: $city \n
 		Message: $message";
-		
+
 		$recipient = "help.regaltheme@gmail.com";
 		$subject = "Contact With MiniPO Admin";
 		$mailheader = "From: $email \r\n";
 		mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-		
+
 	?>
-	
-	
+
+
 </body>
 </html>
